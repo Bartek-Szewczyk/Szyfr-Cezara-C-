1041e1f16e4c332b59e481527449b03736e9d385
Bartek-Szewczyk <ba20001000@gmail.com>
2020-10-05 22:03:46 +0200

    project 1.0

--- Szyfr Cezara/MainWindow.xaml.cs
+++ Szyfr Cezara/MainWindow.xaml.cs
@@ -23,8 +23,120 @@ namespace Szyfr_Cezara
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+
+
         }
 
-       
+
+        public void Szyfruj()
+        {
+            char[] alphabet = new char[] { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż' };
+
+            int key = (int)slValue.Value;
+
+            string orginaltext = text1.Text.ToLower();
+            
+
+            char[] secretText = orginaltext.ToCharArray();
+
+            char[] encryptedText = new char[secretText.Length];
+
+         
+            
+            for (int i = 0; i < secretText.Length; i++)
+            {
+               
+                char secretItem = secretText[i];
+                int index = Array.IndexOf(alphabet, secretItem);
+                int letterPosition = (index += key) % 35;
+                char encriptedCharacter = alphabet[letterPosition];
+                encryptedText[i] = encriptedCharacter;
+
+            }
+
+            string secretTextFinal = String.Join("", encryptedText);
+
+            text2.Text = secretTextFinal;
+
+
+
+
+        }
+
+
+        public void btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (combobox1.Text == "Szyfrowanie")
+            {
+                if (slValue.Value==0)
+                {
+                    text2.Text = text1.Text;
+                }
+                else
+                {
+                    Szyfruj();
+                }
+                
+            }
+            else
+            {
+                if (slValue.Value == 0)
+                {
+                    text2.Text = text1.Text;
+                }
+                else
+                {
+                    Deszyfrowanie();
+                }
+                
+            }
+
+        }
+
+        private void Deszyfrowanie()
+        {
+            char[] alphabet = new char[] { 'ż', 'ź', 'z', 'y', 'x', 'w', 'v', 'u', 't', 'ś', 's', 'r', 'q', 'p', 'ó', 'o', 'ń', 'n', 'm', 'ł', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'ę', 'e', 'd', 'ć', 'c', 'b', 'ą', 'a' };
+
+            int key = (int)slValue.Value;
+
+            string orginaltext = text1.Text.ToLower().Replace(" ", "");
+
+            char[] secretText = orginaltext.ToCharArray();
+
+            char[] encryptedText = new char[secretText.Length];
+
+
+            for (int i = 0; i < secretText.Length; i++)
+            {
+
+                char secretItem = secretText[i];
+                int index = Array.IndexOf(alphabet, secretItem);
+                int letterPosition = (index += key) % 35;
+                char encriptedCharacter = alphabet[letterPosition];
+                encryptedText[i] = encriptedCharacter;
+
+            }
+
+            string secretTextFinal = String.Join("", encryptedText);
+
+            text2.Text = secretTextFinal;
+        }
+
+        private void btnclean_Click(object sender, RoutedEventArgs e)
+        {
+            Clean();
+        }
+
+        private void Clean()
+        {
+            text1.Text = "Wprowadz Tekst";
+            text2.Text = "";
+            slValue.Value = 0;
+            encrypted.IsSelected=true;
+        }
     }
 }
